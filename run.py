6092fb2fb34a451ee12ab3a22b416bba76aaf836
Tanmay Mudholkar <tkmudholkar@gmail.com>
2025-05-10 13:29:31 -0500

    Bugfix: ID used to set volume is different from target used to record audio out.

--- run.py
+++ run.py
@@ -40,10 +40,13 @@ Example of YAML input:
 ...
 """
 
-node_id = None
+node_id     = None
+node_serial = None
 vol_orig_pc = None
 
-def set_volume(node_id, tgt):
+def set_volume(tgt):
+    if node_id is None:
+        raise ValueError
     subprocess.check_call(
         f'wpctl set-volume {node_id} {tgt}%',
         shell=True, executable='/usr/bin/zsh'
@@ -69,19 +72,25 @@ def get_time_in_seconds(time_str: str)\
 
 def parse_serial_id(pw_cli_output: str) -> int:
     re_nodeinfo_begin = re.compile(r'id ([0-9]+), type .*')
-    re_media_class = re.compile(r'media\.class = "Audio/Sink"')
-    re_node_desc = re.compile(r'node\.description = ".*HD Audio Controller Analog Stereo"')
-    node_id = None
+    re_serial         = re.compile(r'object.serial = "([0-9]+)"')
+    re_media_class    = re.compile(r'media\.class = "Audio/Sink"')
+    re_node_desc      = re.compile(r'node\.description = ".*HD Audio Controller Analog Stereo"')
+    node_id             = None
+    node_serial         = None
     media_class_matches = False
-    node_desc_matches = False
+    node_desc_matches   = False
     for line_raw in pw_cli_output.split('\n'):
         line = line_raw.strip()
-        match_obj = re_nodeinfo_begin.match(line)
-        if match_obj:
-            node_id = int(match_obj.group(1))
+        match_nodeinfo_begin = re_nodeinfo_begin.match(line)
+        if match_nodeinfo_begin:
+            node_id = int(match_nodeinfo_begin.group(1))
+            node_serial         = None
             media_class_matches = False
-            node_desc_matches = False
+            node_desc_matches   = False
             continue
+        match_serial = re_serial.match(line)
+        if match_serial:
+            node_serial = int(match_serial.group(1))
         if not media_class_matches:
             if re_media_class.match(line):
                 media_class_matches = True
@@ -90,9 +99,8 @@ def parse_serial_id(pw_cli_output: str) -> int:
                 node_desc_matches = True
         if media_class_matches and node_desc_matches:
             break
-    if node_id is None:
+    if (node_id is None) or (node_serial is None):
         raise ValueError('ERROR: Unable to find audio node ID.')
-    return node_id
 
 def parse_vol_output_pc(vol_output: str) -> int:
     re_match = re.match(r'Volume: (.*)', vol_output)
@@ -113,8 +121,8 @@ def main(args):
         'pw-cli list-objects Node',
         shell=True, executable='/usr/bin/zsh', text=True,
     )
-    node_id = parse_serial_id(pw_cli_output)
-    print(f'Found audio serial number: {node_id}')
+    parse_serial_id(pw_cli_output)
+    print(f'Found audio serial number: {node_serial}')
 
     # get current volume level
     vol_output = subprocess.check_output(
@@ -123,7 +131,7 @@ def main(args):
     )
     vol_orig_pc = parse_vol_output_pc(vol_output)
     # set volume to 100%
-    set_volume(node_id, 100)
+    set_volume(100)
 
     subprocess.check_call(
         'notify-send Get ready...',
@@ -151,7 +159,7 @@ def main(args):
         print(f'Saving track with title {title}, time in seconds: {time_in_seconds}')
         try:
             subprocess.check_call(
-                f'pw-record --target {node_id} {args.out_dir}/{(i+args.idx_min):04}_{title}.wav',
+                f'pw-record --target "{node_serial}" {args.out_dir}/{(i+args.idx_min):04}_{title}.wav',
                 shell=True,
                 executable='/usr/bin/zsh',
                 timeout=time_in_seconds,
@@ -159,13 +167,13 @@ def main(args):
         except subprocess.TimeoutExpired:
             # always should end here
             pass
-    set_volume(node_id, vol_orig_pc)
+    set_volume(vol_orig_pc)
     print('All Done.')
 
 def exit_gracefully() -> None:
     if (node_id is None) or (vol_orig_pc is None):
         return
-    set_volume(node_id, vol_orig_pc)
+    set_volume(vol_orig_pc)
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT,  exit_gracefully)
